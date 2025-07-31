d872749f06c88c8d254eb227dd71d4baedfc3b20
galgeorge86 <50632904+galgeorge86@users.noreply.github.com>
2025-07-31 16:56:21 +0300

    Fixing prisma integration

--- utils/prisma.ts
+++ utils/prisma.ts
@@ -1,11 +1,15 @@
 import { PrismaClient } from '@/app/generated/prisma/client'
+import { PrismaNeon } from '@prisma/adapter-neon'
 import { withAccelerate } from '@prisma/extension-accelerate'
+
+const connectionString = process.env.DATABASE_URL
+const adapter = new PrismaNeon({connectionString})
 
 const globalForPrisma = global as unknown as { 
     prisma: PrismaClient
 }
 
-const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+const prisma = globalForPrisma.prisma || new PrismaClient({adapter}).$extends(withAccelerate())
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
